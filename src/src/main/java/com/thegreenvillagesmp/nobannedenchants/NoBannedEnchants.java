66aa3d60097d227c0ca594c10843c146a0afddc0
Ariel Walter <arielwalter79@gmail.com>
2026-09-17 17:22:17 +0300

    Add NoBannedEnchants mod initializer

---
+++ src/src/main/java/com/thegreenvillagesmp/nobannedenchants/NoBannedEnchants.java
@@ -0,0 +1,13 @@
+package com.thegreenvillagesmp.nobannedenchants;
+
+import net.fabricmc.api.ModInitializer;
+
+public class NoBannedEnchants implements ModInitializer {
+
+    public static final String MOD_ID = "nobannedenchants";
+
+    @Override
+    public void onInitialize() {
+        System.out.println("[NoBannedEnchants] Loaded!");
+    }
+}
